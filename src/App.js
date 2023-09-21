a16175ac61d9fe36a0ad72c49dc9f12ad9416d1c
Zachary Ringwood <zringwood@gmail.com>
2023-09-21 19:54:13 -0400

    Initial commit. First prototype done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Display from './components/Display/Display';
 
+
+const SIZE = 1000
 function App() {
+  const sprites = []
+  for(let i = 0;i<100;i++){
+    sprites.push({
+      x:Math.random(),
+      y:Math.random(),
+      dir:Math.random()*2*Math.PI,
+      color:`rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`,
+      type:Math.trunc(Math.random() * 3)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Display arr={sprites} width={SIZE} height={SIZE}/>
   );
 }
 
---
+++ src/components/Display/Display.js
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from "react";
+
+const MILLIS = 1000;
+const FRAMERATE = 30;
+const SPEED = 0.0001;
+const COLLISION_BOX = 0.010;
+
+export default function Display({arr, width, height}){
+const canvasRef = useRef(null)
+const [sprites, setSprites] = useState(arr)
+
+useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    context.clearRect(0, 0, width, height)
+    context.fillStyle = "#000000"
+    context.strokeRect(0, 0, width, height)
+    sprites.forEach(sprite => {
+        if(sprite.type === 0)
+            context.fillStyle = "#FF0000"
+        if(sprite.type === 1)
+            context.fillStyle = "#00FF00"
+        if(sprite.type === 2)
+            context.fillStyle = "#0000FF"
+        context.fillRect(Math.round(sprite.x * width), Math.round(sprite.y * height), COLLISION_BOX*width, COLLISION_BOX*height);
+    });
+},[sprites, width, height])
+setInterval(() => {
+    sprites.forEach(sprite => {
+        sprite.x = sprite.x + SPEED * Math.cos(sprite.dir);
+        sprite.y = sprite.y + SPEED * Math.sin(sprite.dir);
+        if(sprite.x < 0 || sprite.y < 0){
+            sprite.dir += Math.PI/4;
+        }
+        if(sprite.x > 1 || sprite.y > 1 ){
+            sprite.dir -= Math.PI/4;
+        }
+        sprites.forEach(sprite0 => {
+            if(Math.abs(sprite.x - sprite0.x) < COLLISION_BOX && Math.abs(sprite.y - sprite0.y) < COLLISION_BOX){
+                sprite.dir += Math.PI;
+                sprite0.dir += Math.PI;
+                if(sprite.type === 0 && sprite0.type === 1){
+                    sprite.type = 1
+                }else if(sprite.type === 0 && sprite0.type === 2){
+                    sprite0.type = 0
+                }else if(sprite.type === 1 && sprite0.type === 2){
+                    sprite.type = 2
+                }else if(sprite.type === 1 && sprite0.type === 0){
+                    sprite0.type = 1
+                }else if(sprite.type === 2 && sprite0.type === 0){
+                    sprite.type = 0
+                }else if(sprite.type === 2 && sprite0.type === 1){
+                    sprite0.type = 2
+                }
+            }
+        })
+    })
+   setSprites([...sprites])
+   }, MILLIS/FRAMERATE)
+    return <canvas ref = {canvasRef} width={width} height={height} style={{width:`${width}px`, height:`${height}px`}} onClick={e => console.log(e)}/>
+}
